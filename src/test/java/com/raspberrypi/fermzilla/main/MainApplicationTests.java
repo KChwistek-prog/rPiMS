49b403c8745c748fa93d56b73d6345a660ea99e8
Krzysztof <vescus@gmail.com>
2021-08-01 09:39:26 +0200

    Add setting and build files

---
+++ src/test/java/com/raspberrypi/fermzilla/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspberrypi.fermzilla.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
