914a3a1cc39ba3476a48e47a6c6559d26337f48d
K.Chwistek <k.chwistek@outlook.com>
2021-08-14 08:25:04 +0200

    bash script extend

--- moveToPi.sh
+++ moveToPi.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env bash
 
 FILENAME="rPiMS.jar"
+BUILDPATH="build/libs"
+
+stopApp(){
+  echo "Killing app"
+  ssh pi@192.168.0.101 "cd public; sh ./starter.sh stop"
+  echo "RiP - $FILENAME"
+}
+
+build_project(){
+  echo "Building"
+  sh ./gradlew clean build
+  echo "Build completed"
+}
 
 rename_file(){
   echo "Renaming to $FILENAME"
-  cp build/libs/main-0.0.1-SNAPSHOT.jar build/libs/$FILENAME
-}
-copy_file(){
-  echo "Moving file to RaspberryPi"
-  mv build/libs/$FILENAME z:/
+  cp $BUILDPATH/main-0.0.1-SNAPSHOT.jar $BUILDPATH/$FILENAME
 }
 
-echo "Starting"
-rename_file
-copy_file
-echo "Operation completed"
+copy_file(){
+  echo "Moving file to RaspberryPi"
+  mv $BUILDPATH/$FILENAME z:/
+  echo "Operation completed"
+}
+
+startApp(){
+  echo "Starting App"
+  ssh pi@192.168.0.101 "cd public; sh ./starter.sh start"
+  echo "App started"
+}
+
+moveToPi(){
+  echo "Starting script"
+  stopApp
+  build_project
+  rename_file
+  copy_file
+  startApp
+}
+
+moveToPi
