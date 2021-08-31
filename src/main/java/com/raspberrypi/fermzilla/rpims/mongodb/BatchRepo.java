dcf71ed9bf5e23a9982389e91213a4f9fe80d92a
KChwistek <vescus@gmail.com>
2021-08-31 21:47:50 +0200

    mongodb dependency removed, mongodb files moved to rpims-monitor

--- src/main/java/com/raspberrypi/fermzilla/rpims/mongodb/BatchRepo.java
+++
@@ -1,9 +0,0 @@
-package com.raspberrypi.fermzilla.rpims.mongodb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BatchRepo extends MongoRepository<BatchPojo, String> {
-
-}
